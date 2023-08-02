4ee4f87ec2c213854ed2f57c72902ff4aa80a6f2
Dan Zheng <danielzheng@google.com>
2023-08-02 13:55:15 -0700

    Clean up absl flag parsing.
    Use `FlagHolder` values returned via `flags.DEFINE_*` calls, following best practices.

--- triangulate/main.py
+++ triangulate/main.py
@@ -22,27 +22,27 @@ from triangulate import core
 Localiser = core.Localiser
 Environment = core.Environment
 
-flags.DEFINE_string(
+_SUBJECT = flags.DEFINE_string(
     "subject",
     None,
     help="the name of a buggy file",
     required=True,
     short_name="p",
 )
-flags.DEFINE_string(
+_BUG_TRIGGERING_INPUT = flags.DEFINE_string(
     "bug_triggering_input",
     None,
     required=True,
     short_name="b",
     help="a bug-triggering input",
 )
-flags.DEFINE_integer(
+_LOGLEVEL = flags.DEFINE_integer(
     "loglevel",
     0,
     short_name="l",
     help="Set logging level (default: INFO)",
 )
-flags.DEFINE_integer(
+_BUG_TRAP = flags.DEFINE_integer(
     "bug_trap",
     0,
     short_name="t",
@@ -50,7 +50,7 @@ flags.DEFINE_integer(
 )
 # During burnin, the program stores outputs for later use to checking
 # whether injecting/executing probes has changed program semantics.
-flags.DEFINE_integer(
+_BURNIN = flags.DEFINE_integer(
     "burnin",
     0,
     short_name="n",
@@ -60,13 +60,13 @@ flags.DEFINE_integer(
         "zero (the default) disables burnin."
     ),
 )
-flags.DEFINE_integer(
+_MAX_STEPS = flags.DEFINE_integer(
     "max_steps",
     10,
     short_name="m",
     help="maximum simulation steps",
 )
-flags.DEFINE_string(
+_PROBE_OUTPUT_FILENAME = flags.DEFINE_string(
     "probe_output_filename",
     "__probeOutput.dmp",
     short_name="o",
@@ -80,34 +80,32 @@ def main(argv):
   if len(argv) < 1:
     raise app.UsageError("Too few command-line arguments.")
 
-  FLAGS = flags.FLAGS # pylint: disable=invalid-name
-  logging.set_verbosity(FLAGS.loglevel)
+  logging.set_verbosity(_LOGLEVEL.value)
 
-  if not 0 <= FLAGS.burnin < 1:
+  if not 0 <= _BURNIN.value < 1:
     err_template = "Error: burnin period must fall into the interval [0,1)."
     logging.error(err_template)
     raise ValueError(err_template)
 
-  if not FLAGS.subject:
-    FLAGS.subject = input(
-        "Please enter the name of the subject program: "
-    )
+  if _SUBJECT.present:
+    subject = _SUBJECT.value
+  else:
+    subject = input("Please enter the name of the buggy program: ")
 
-  # Cannot use ** as Abseil populates its dictionary with unrelated flags
-  flags_dict = FLAGS.flag_values_dict()
   env = Environment(
-    subject = flags_dict["subject"],
-    bug_triggering_input = flags_dict["bug_triggering_input"],
-    bug_trap = flags_dict["bug_trap"],
-    burnin = flags_dict["burnin"],
-    max_steps = flags_dict["max_steps"],
-    probe_output_filename = flags_dict["probe_output_filename"],
-    loglevel = FLAGS.loglevel
+      subject=subject,
+      bug_triggering_input=_BUG_TRIGGERING_INPUT.value,
+      bug_trap=_BUG_TRAP.value,
+      burnin=_BURNIN.value,
+      max_steps=_MAX_STEPS.value,
+      probe_output_filename=_PROBE_OUTPUT_FILENAME.value,
+      loglevel=_LOGLEVEL.value,
   )
   localiser = Localiser(env)
 
   while not env.terminate():
     env.update(localiser.pick_action(env.state, env.reward()))
+
 
 if __name__ == "__main__":
   app.run(main)
